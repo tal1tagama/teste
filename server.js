2b83bc6617d124b0fef13435c3296263df5827ab
tal1taGama <0001013061@senaimgaluno.com.br>
2025-10-06 19:25:12 -0300

    +

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import cors from '@fastify/cors'
+import { app }from './app.js'
 
 async function server(){
-    App.register(cors,{
+    app.register(cors,{
         origin:true,
         method: ["GET", "POST", "PATCH", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"]
+        allowedHeaders: ["Content-Type"]
     })
+
+    app.listen ({
+        host:'0.0.0.0',
+        port: 3333
+    }).then(()=>{
+        console.log('HTTP is running on PORT 3333')
+    }
+)
 }
+
+server();
